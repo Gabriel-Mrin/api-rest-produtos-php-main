4aff22383bba089b5a5342e8762644a55ca82c43
Gabriel-Mrin <aluno.mrt@gmail.com>
2026-05-25 19:32:32 -0300

    Criação de categoria e clientes em model

---
+++ api/models/Clientes .php
@@ -0,0 +1,63 @@
+<?php
+
+namespace api\models;
+
+use api\core\Database;
+use PDO;
+
+class Clientes {
+
+    public static function findAll() {
+        $conn = new Database();
+
+        $result = $conn->executeQuery(
+            'SELECT * FROM clientes'
+        );
+
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function findByCpf(string $cpf) {
+
+        $conn = new Database();
+
+        $result = $conn->executeQuery(
+            'SELECT * FROM clientes WHERE cpf = :CPF LIMIT 1',
+            array(
+                ':CPF' => $cpf
+            )
+        );
+
+        return $result->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public static function updateByCpf(string $cpf, array $data) {
+
+        $conn = new Database();
+
+        return $conn->executeQuery(
+            'UPDATE clientes
+             SET
+                nome = :NOME,
+                email = :EMAIL
+             WHERE cpf = :CPF',
+            array(
+                ':NOME' => $data['nome'],
+                ':EMAIL' => $data['email'],
+                ':CPF' => $cpf
+            )
+        );
+    }
+
+    public static function deleteByCpf(string $cpf) {
+
+        $conn = new Database();
+
+        return $conn->executeQuery(
+            'DELETE FROM clientes WHERE cpf = :CPF',
+            array(
+                ':CPF' => $cpf
+            )
+        );
+    }
+}
