42b3ee61057bd824c3c8c1a0d410218b13a63dd0
Gabriel-Mrin <aluno.mrt@gmail.com>
2026-05-27 15:17:19 -0300

    criando creat views  pra categoria

---
+++ api/views/Category/create.php
@@ -0,0 +1,15 @@
+<h1>Nova Categoria</h1>
+
+<form method="POST">
+
+  <input 
+    type="text" 
+    name="nome"
+    placeholder="Nome da categoria"
+  >
+
+  <button type="submit">
+    Cadastrar
+  </button>
+
+</form>
