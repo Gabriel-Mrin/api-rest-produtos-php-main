3ef9ea27c477f816c0490efe10d3aa1c0cad2745
Gabriel-Mrin <aluno.mrt@gmail.com>
2026-05-26 22:28:50 -0300

    criação show views categoria

---
+++ api/views/Category/show.php
@@ -0,0 +1,25 @@
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col-8 offset-2" style="margin-top:100px">
+
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Nome</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($data as $category) { ?>
+            <tr>
+              <td><?= $category['id'] ?></td>
+              <td><?= $category['nome'] ?></td>
+            </tr>
+            <?php }?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</main>
